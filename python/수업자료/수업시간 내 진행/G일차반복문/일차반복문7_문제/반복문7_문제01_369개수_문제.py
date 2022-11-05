ed0087671b7822aabd5da2425428f076a5402600
wisline97 <wisdomline.an@gmail.com>
2022-11-05 17:05:48 +0900

    221105 주말공부

--- python/수업자료/수업시간 내 진행/G일차반복문/일차반복문7_문제/반복문7_문제01_369개수_문제.py
+++ python/수업자료/수업시간 내 진행/G일차반복문/일차반복문7_문제/반복문7_문제01_369개수_문제.py
@@ -13,3 +13,40 @@
             98 1
             99 2
 '''
+
+min = 1
+max = 100
+count = 0
+
+
+
+while min <= 100:
+    min십의자리 = min // 10
+    min일의자리 = min % 10
+
+    if min십의자리 % 3 == 0 or min일의자리%3 == 0:
+        count = 1
+
+    if min>=10 and min십의자리 % 3 == 0 and min일의자리%3 == 0:
+        count = 2
+
+    if min<10 and min%3 == 0:
+        print(min, count)
+
+    if min>=10 and min일의자리 % 3 == 0 and min일의자리 != 0:
+        print(min, count)
+
+    min += 1
+
+
+'''
+while min <= 100:
+    if min < 10 and min % 3 == 0:
+        print(min, count+1)
+    if min >= 10 and (min % 10)%3 == 0 :
+        print(min, count+1)
+    if (min // 10) % 3 ==0 and (min % 10)%3 == 0 :
+        print(min, count+2)
+
+    min += 1
+'''
